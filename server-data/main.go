c16b44a95aa1cc951a7a87b5d7a09b772fc00ad6
Vivek Shankar <vshankar@progress.com>
2021-06-11 19:44:04 +0530

    all files commited concurrency added

--- server-data/main.go
+++ server-data/main.go
@@ -10,173 +10,347 @@ import (
 	"crypto/tls"
 	"encoding/json"
 	"errors"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
+	"math/rand"
 	"net/http"
 	"text/template"
 	"time"
+
+	uuid "github.com/satori/go.uuid"
 )
+
+type comp struct {
+	ProfileVersion,
+	ProfileSHA,
+	EndPoint,
+	Environment,
+	NodeName,
+	NodeUUID,
+	PlatformName,
+	PlatformRelease,
+	ProfileID1,
+	ProfileID2,
+	ProfileID3,
+	ProfileID4,
+	ProfileID5,
+	ProfileID6,
+	ProfileID7,
+	ProfileID8,
+	ProfileID9,
+	ProfileID10,
+	ProfileID11,
+	ProfileID12,
+	ProfileID13,
+	ProfileID14,
+	ProfileName,
+	Recipe1,
+	Recipe2,
+	Recipe3,
+	Roles1,
+	Roles2,
+	Roles3,
+	ReportUUID string
+}
+
+// Prepare some data to insert into the template.
+type node struct {
+	EntityUUID,
+	NodeName,
+	Hostnamestr,
+	IpAddress,
+	Hostname,
+	EndTime,
+	StartTime,
+	Recipe1,
+	Recipe2,
+	Recipe3,
+	ID,
+	Platform,
+	Roles,
+	ChefEnvironment,
+	Attr,
+	NormalTags,
+	OrganizationName,
+	CookbookName,
+	CookbookVersion,
+	CookbookID,
+	RunID string
+}
+
+const charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+var seededRand *rand.Rand = rand.New(
+	rand.NewSource(time.Now().UnixNano()))
+
+func String(length int) string {
+	return StringWithCharset(length, charset)
+}
+
+func StringWithCharset(length int, charset string) string {
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[seededRand.Intn(len(charset))]
+	}
+	return string(b)
+}
+
+func generateIpAddress() string {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	data := fmt.Sprintf("%d.%d.%d.%d", r.Intn(255), r.Intn(255), r.Intn(255), r.Intn(255))
+	return data
+}
+
+func randomserialNumber() string {
+	serialNumber := uuid.NewV4()
+	return fmt.Sprintf("%s", serialNumber)
+}
+
+func randInt(min int, max int) int {
+	rand.Seed(time.Now().UnixNano())
+	return min + rand.Intn(max-min)
+}
 
 func main() {
 
-	type comp struct {
-		EndPoint,
-		Environment,
-		NodeName,
-		NodeUUID,
-		PlatformName,
-		PlatformRelease,
-		ProfileID1,
-		ProfileID2,
-		ProfileID3,
-		ProfileID4,
-		ProfileID5,
-		ProfileID6,
-		ProfileID7,
-		ProfileID8,
-		ProfileID9,
-		ProfileID10,
-		ProfileID11,
-		ProfileID12,
-		ProfileID13,
-		ProfileID14,
-		ProfileName,
-		Recipe1,
-		Recipe2,
-		Recipe3,
-		Roles1,
-		Roles2,
-		Roles3,
-		ReportUUID string
+	err := false
+	numberOfElements := flag.Int("numberOfElement", 1, "number of elements")
+	url := flag.String("url", "", "automate url")
+	apiToken := flag.String("token", "", "automate token")
+	dataType := flag.String("data", "node", "type of data to send: node or compliance")
+	concurrency := flag.Int("concurrency", 1, "Number of concurrent requests to run")
+
+	maxGoroutines := concurrency
+
+	flag.Parse()
+	if *numberOfElements <= 0 {
+		err = true
+		fmt.Println("Please enter valid number")
+	}
+	if *url == "" {
+		err = true
+		fmt.Println("url should not be empty")
+	}
+	if *apiToken == "" {
+		err = true
+		fmt.Println("token should not be empty")
+	}
+	if *dataType != "node" && *dataType != "compliance" {
+		err = true
+		fmt.Println("data should be either node or compliance")
 	}
 
-	// Prepare some data to insert into the template.
-	type Recipient struct {
-		Hostname,
-		EndTime,
-		StartTime,
-		Recipe1,
-		Recipe2,
-		Recipe3,
-		ID,
-		Platform,
-		Roles,
-		ChefEnvironment,
-		Attr,
-		NormalTags,
-		OrganizationName,
-		CookbookName,
-		CookbookVersion,
-		CookbookID,
-		RunID string
-	}
-	var recipients = []Recipient{
-		{
-			"hostname",
-			"2021-06-10T11:55:52Z",
-			"2021-06-10T11:47:52Z",
-			"nginx::default",
-			"tomcat::default",
-			"yum::default",
-			"c2fb9ee4-13ec-4c48-a495-8d6a52710e33",
-			"ubuntu",
-			"extra_who_died_on_ER",
-			"grassland",
-			"something",
-			"network_device",
-			"The Defenders",
-			"nginx", "0.1.0",
-			"chef-sugar",
-			"c2fb9ee4-13ec-4c48-a495-8d6a52710e33",
-		},
+	if !err {
+		makeRequest(*numberOfElements, *url, *apiToken, *dataType, *maxGoroutines)
 	}
 
-	var data = []comp{
-		{
-			"2021-06-11T07:02:25Z",
-			"DevSec Dev_Gamma1",
-			"chef-load-violet-waxwing-yellow",
-			"2a463196-970c-3ecc-97bc-46945d0844da",
-			"debian",
-			"8.11",
-			"apache-01",
-			"apache-02",
-			"apache-03",
-			"apache-04",
-			"apache-05",
-			"apache-06",
-			"apache-07",
-			"apache-08",
-			"apache-09",
-			"apache-10",
-			"apache-11",
-			"apache-12",
-			"apache-13",
-			"apache-14",
-			"DevSec Apache Baseline",
-			"linux::harden",
-			"tomcat",
-			"tomcat::setup",
-			"base_windows",
-			"windows-hardening",
-			"best.role.ever",
-			"5fb3688a-90e3-4571-bed6-0067432d1878",
-		},
-	}
+	// var recipients = []Recipient{
+	// 	{
+	// 		"vivek-test",
+	// 		"ip-22.22.22.3",
+	// 		"22.22.22.3",
+	// 		"hostname",
+	// 		"2021-06-10T11:55:52Z",
+	// 		"2021-06-10T11:47:52Z",
+	// 		"nginx::default",
+	// 		"tomcat::default",
+	// 		"yum::default",
+	// 		"c2fb9ee4-13ec-4c48-a495-8d6a52710e33",
+	// 		"ubuntu",
+	// 		"extra_who_died_on_ER",
+	// 		"grassland3",
+	// 		"something",
+	// 		"network_device",
+	// 		"The Defenders",
+	// 		"nginx", "0.1.0",
+	// 		"chef-sugar",
+	// 		"c2fb9ee4-13ec-4c48-a495-8d6a52710e33",
+	// 	},
+	// }
 
-	// Create a new template and parse the letter into it.
-	tmpl := template.New("node.tmpl")
-
-	t, err := tmpl.ParseFiles("node.tmpl")
-	if err != nil {
-		panic(err)
-	}
+	// var data = []comp{
+	// 	{
+	// 		"2021-06-11T07:02:25Z",
+	// 		"DevSec Dev_Gamma1",
+	// 		"chef-load-violet-waxwing-yellow",
+	// 		"2a463196-970c-3ecc-97bc-46945d0844da",
+	// 		"debian",
+	// 		"8.11",
+	// 		"apache-01",
+	// 		"apache-02",
+	// 		"apache-03",
+	// 		"apache-04",
+	// 		"apache-05",
+	// 		"apache-06",
+	// 		"apache-07",
+	// 		"apache-08",
+	// 		"apache-09",
+	// 		"apache-10",
+	// 		"apache-11",
+	// 		"apache-12",
+	// 		"apache-13",
+	// 		"apache-14",
+	// 		"DevSec Apache Baseline",
+	// 		"linux::harden",
+	// 		"tomcat",
+	// 		"tomcat::setup",
+	// 		"base_windows",
+	// 		"windows-hardening",
+	// 		"best.role.ever",
+	// 		"5fb3688a-90e3-4571-bed6-0067432d1878",
+	// 	},
+	// }
 
 	// Execute the template for each recipient.
-	var tpl bytes.Buffer
-	for _, r := range recipients {
-		err := t.Execute(&tpl, r)
-		if err != nil {
-			log.Println("executing template:", err)
-		}
-	}
 
 	// fmt.Println(tpl.String())
 
-	// Create a new template and parse the letter into it.
-	tmplc := template.New("compliance.tmpl")
-
-	tr, errr := tmplc.ParseFiles("compliance.tmpl")
-	if errr != nil {
-		panic(err)
-	}
-
 	// Execute the template for each recipient.
-	var tplc bytes.Buffer
-	for _, r := range data {
-		err := tr.Execute(&tplc, r)
-		if err != nil {
-			log.Println("executing template:", err)
-		}
-	}
 
-	mydoc := map[string]interface{}{}
-	fmt.Println("here::::1")
+	// mydoc := map[string]interface{}{}
+	// fmt.Println("here::::1", tpl.String())
 
-	err = json.Unmarshal([]byte(tplc.String()), &mydoc)
-	if err != nil {
-		fmt.Println("Error::::::", err)
-		return
-	}
-	fmt.Println("here::::2")
-
-	err = sendNotification(tplc.String(), "https://ec2-13-233-86-54.ap-south-1.compute.amazonaws.com/api/v0/events/data-collector", "OTsmSLhhLLeSIrNN6AloGDykP-M=")
-	if err != nil {
-		fmt.Println("Error", err)
-	}
+	// err := json.Unmarshal([]byte(tplc.String()), &mydoc)
+	// if err != nil {
+	// 	fmt.Println("Error::::::", err)
+	// 	return
+	// }
+	// fmt.Println("here::::2")
 
 	// fmt.Println(tplc.String())
+
+}
+
+func makeRequest(numberOfElements int, endpoint string, apiToken string, dataType string, maxGoroutines int) {
+
+	if numberOfElements < maxGoroutines {
+		maxGoroutines = 1
+	}
+	guard := make(chan struct{}, maxGoroutines)
+	doneChannel := make(chan bool)
+
+	// endpoint := "https://ec2-13-233-86-54.ap-south-1.compute.amazonaws.com/api/v0/events/data-collector"
+	// apiToken := "OTsmSLhhLLeSIrNN6AloGDykP-M="
+
+	var nodeData = make([]node, numberOfElements)
+	var complianceData = make([]comp, numberOfElements)
+
+	if dataType == "node" {
+		for i := 0; i < numberOfElements; i++ {
+			nodeData[i] = node{
+				randomserialNumber(),
+				String(10),
+				"ip-" + generateIpAddress(),
+				generateIpAddress(),
+				"testhost" + String(8),
+				"2021-06-10T11:55:52Z",
+				"2021-06-10T11:47:52Z",
+				"nginx::default" + String(3),
+				"tomcat::default" + String(3),
+				"yum::default" + String(3),
+				randomserialNumber(),
+				"ubuntu" + String(2),
+				"test" + String(3),
+				"grow" + String(10),
+				String(10),
+				"network_device" + String(3),
+				"The " + String(8),
+				String(6),
+				"0.1." + fmt.Sprintf("%d", randInt(0, 10)),
+				"chef-" + String(5),
+				randomserialNumber(),
+			}
+		}
+
+		for _, r := range nodeData {
+			guard <- struct{}{}
+			go func(guard chan struct{}, r node, doneChannel chan bool) {
+				var tpl bytes.Buffer
+				// Create a new template and parse the letter into it.
+				tmpl := template.New("node.tmpl")
+				t, err := tmpl.ParseFiles("node.tmpl")
+				if err != nil {
+					panic(err)
+				}
+				err = t.Execute(&tpl, r)
+				if err != nil {
+					log.Println("executing template:", err)
+				}
+				err = sendNotification(tpl.String(), endpoint, apiToken)
+				if err != nil {
+					fmt.Println("Error", err)
+				}
+				<-guard
+				doneChannel <- true
+			}(guard, r, doneChannel)
+		}
+
+	} else if dataType == "compliance" {
+		for i := 0; i < numberOfElements; i++ {
+			complianceData[i] = comp{
+				"2.1." + fmt.Sprintf("%d", randInt(0, 10)),
+				String(5),
+				"2021-06-11T07:02:25Z",
+				"DevSec " + String(3) + " " + String(5),
+				"chef-test-violet-waxwing-yellow-" + String(6),
+				randomserialNumber(),
+				"debian",
+				"8." + fmt.Sprintf("%d", randInt(10, 20)),
+				"apache-01",
+				"apache-02",
+				"apache-03",
+				"apache-04",
+				"apache-05",
+				"apache-06",
+				"apache-07",
+				"apache-08",
+				"apache-09",
+				"apache-10",
+				"apache-11",
+				"apache-12",
+				"apache-13",
+				"apache-14",
+				"DevSec Apache Baseline" + String(6),
+				"linux::harden",
+				"tomcat" + String(5),
+				"tomcat::setup" + String(5),
+				"base_windows" + String(5),
+				"windows-hardening" + String(5),
+				"best.role.ever" + String(5),
+				randomserialNumber(),
+			}
+		}
+
+		for _, r := range complianceData {
+
+			guard <- struct{}{}
+
+			go func(guard chan struct{}, r comp, doneChannel chan bool) {
+				var tplc bytes.Buffer
+				tmplc := template.New("compliance.tmpl")
+
+				tr, errr := tmplc.ParseFiles("compliance.tmpl")
+				if errr != nil {
+					panic(errr)
+				}
+				err := tr.Execute(&tplc, r)
+				if err != nil {
+					log.Println("executing template:", err)
+				}
+				err = sendNotification(tplc.String(), endpoint, apiToken)
+				if err != nil {
+					fmt.Println("Error", err)
+				}
+				<-guard
+				doneChannel <- true
+			}(guard, r, doneChannel)
+		}
+	}
+	for i := 0; i < numberOfElements; i++ {
+		<-doneChannel
+	}
 
 }
 
